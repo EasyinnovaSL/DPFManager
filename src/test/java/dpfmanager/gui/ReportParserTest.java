e25f1f0c2566d29eb004df939dd2d41ca82bf256
Victor Muñoz <victormunoz@easyinnova.com>
2016-04-04 12:30:39 +0200

    Travis testfix

--- src/test/java/dpfmanager/gui/ReportParserTest.java
+++ src/test/java/dpfmanager/gui/ReportParserTest.java
@@ -59,6 +59,7 @@ public class ReportParserTest extends ApplicationTest {
 
     // Go to reports and check them
     clickOnAndReloadTop("#butReports","#pane-reports");
+    waitForTable("#tabReports");
     TableView<ReportRow> table = (TableView) scene.lookup("#tabReports");
     Assert.assertEquals("Reports table rows", Math.min(nReports + 3, ReportsModel.reports_loaded), table.getItems().size());
     checkValidRow(table.getItems().get(0), "XML");  //Xml
