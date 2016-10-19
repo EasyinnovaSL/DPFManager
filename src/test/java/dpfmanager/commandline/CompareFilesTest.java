28ac0a212b14177a214bf6d13ce33bde065e12a9
Adrià Llorens <adria_llo@hotmail.com>
2016-10-19 11:53:37 +0200

    no message

---
+++ src/test/java/dpfmanager/commandline/CompareFilesTest.java
@@ -0,0 +1,36 @@
+package dpfmanager.commandline;
+
+import dpfmanager.shell.core.app.MainConsoleApp;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.List;
+
+/**
+ * Created by easy on 13/10/2015.
+ */
+public class CompareFilesTest extends CommandLineTest {
+
+
+  @Test
+  public void fullTestPeriodicalCheck() throws Exception {
+    String expected = "18";
+    String current = "17";
+    String expectedPath = "C:\\Users\\Roser\\DPF Manager\\reports\\20161019\\"+expected+"\\summary.xml";
+    String currentPath = "C:\\Users\\Roser\\DPF Manager\\reports\\20161019\\"+current+"\\summary.xml";
+    File expectedFile = new File(expectedPath);
+    File currentFile = new File(currentPath);
+
+    Assert.assertEquals("The files differ!",
+        FileUtils.readFileToString(expectedFile, "utf-8"),
+        FileUtils.readFileToString(currentFile, "utf-8"));
+
+  }
+
+}
