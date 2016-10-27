b3694b0196b438e6a4e65b2fcb54e0424ef0e192
Victor Muñoz <victormunoz@easyinnova.com>
2016-10-27 11:05:38 +0200

    Removed unused dependencies

--- src/main/java/dpfmanager/conformancechecker/tiff/policy_checker/Schematron.java
+++ src/main/java/dpfmanager/conformancechecker/tiff/policy_checker/Schematron.java
@@ -26,83 +26,19 @@ import dpfmanager.conformancechecker.tiff.implementation_checker.rules.model.Ass
 import dpfmanager.conformancechecker.tiff.implementation_checker.rules.model.ImplementationCheckerObjectType;
 import dpfmanager.conformancechecker.tiff.implementation_checker.rules.model.RuleType;
 import dpfmanager.conformancechecker.tiff.implementation_checker.rules.model.RulesType;
-import dpfmanager.shell.core.DPFManagerProperties;
 
 import com.easyinnova.tiff.model.TiffDocument;
-
-import net.sf.saxon.TransformerFactoryImpl;
-
-import org.apache.camel.CamelContext;
-import org.apache.camel.ProducerTemplate;
-import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.component.mock.MockEndpoint;
-import org.apache.camel.component.schematron.constant.Constants;
-import org.apache.camel.component.schematron.processor.ClassPathURIResolver;
-import org.apache.camel.component.schematron.processor.SchematronProcessor;
-import org.apache.camel.component.schematron.processor.SchematronProcessorFactory;
-import org.apache.camel.component.schematron.processor.TemplatesFactory;
-import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.test.junit4.CamelTestSupport;
-import org.apache.tools.ant.filters.StringInputStream;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
-
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Templates;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 /**
  * Created by easy on 21/09/2015.
  */
 public class Schematron {
   /**
-   * The Context.
-   */
-  CamelContext context;
-  /**
-   * The Template.
-   */
-  ProducerTemplate template;
-
-  /**
    * Instantiates a new Schematron.
    */
   public Schematron()
   {
-    context = new DefaultCamelContext();
-    template = context.createProducerTemplate();
-  }
-
-  /**
-   * Convert string.
-   *
-   * @param txt the txt
-   * @return the string
-   */
-  String convert(String txt) {
-    String converted = txt;
-    converted = converted.replace("<","#PP#");
-    converted = converted.replace(">","#GG#");
-    return converted;
   }
 
   public Validator testXMLnoSchematron(TiffDocument tiffModel, Rules rules) throws Exception {
@@ -185,142 +121,5 @@ public class Schematron {
     validation.validate(model, rulesObj, false);
 
     return validation;
-  }
-
-  /**
-   * Test xml string.
-   *
-   * @param xmlFile the xml file
-   * @param rules   the rules
-   * @return the string
-   * @throws Exception the exception
-   */
-  public String testXML(String xmlFile, Rules rules) throws Exception {
-    InputStream sc = getInputStream("sch/rules.sch");
-    if (sc != null) {
-      Document doc = readXml(sc);
-
-      // Reset
-      NodeList nodes = doc.getElementsByTagName("rule");
-      while (nodes.getLength() > 0) {
-        Element el = (Element) nodes.item(0);
-        el.getParentNode().removeChild(el);
-      }
-
-      // Insert rules
-      Element pattern = (Element) doc.getElementsByTagName("pattern").item(0);
-      if (rules != null) {
-        for (Rule r : rules.getRules()) {
-          Element newrule = doc.createElementNS(pattern.getNamespaceURI(), "rule");
-          newrule.setAttribute("context", r.getTag());
-
-          Element assertion;
-          if (r.getWarning())
-            assertion = doc.createElementNS(newrule.getNamespaceURI(), "report");
-          else
-            assertion = doc.createElementNS(newrule.getNamespaceURI(), "assert");
-          String sval = r.getValue();
-          if (r.getType().equals("string")) sval = "'" + sval + "'";
-          String stest = "";
-          if (sval.contains(";")) {
-            for (String spart : sval.substring(1, sval.length() - 1).split(";")) {
-              if (stest.length() > 0) stest += " or ";
-              stest += "@" + r.getTag() + " " + convert(r.getOperator()) + " '" + spart + "'";
-            }
-          } else {
-            stest = "@" + r.getTag() + " " + convert(r.getOperator()) + " " + sval;
-          }
-          assertion.setAttribute("test", stest);
-          assertion.setTextContent("Invalid " + r.getTag() + ": #PP2#value-of select=\"@" + r.getTag() + "\"/#GG#");
-
-          newrule.appendChild(assertion);
-
-          pattern.appendChild(newrule);
-        }
-      }
-
-      TransformerFactory transformerFactory = TransformerFactory.newInstance();
-      Transformer transformer = transformerFactory.newTransformer();
-      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-      transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2");
-      DOMSource source = new DOMSource(doc);
-
-      StringWriter writer = new StringWriter();
-      transformer.transform(source, new StreamResult(writer));
-      String s = writer.toString();
-      s = s.replace("#PP#", "&lt;").replace("#GG#", ">").replace("#PP2#", "<");
-
-      String report = testXML(xmlFile, s);
-      return report;
-    } else {
-      return "";
-    }
-  }
-
-  /**
-   * Read xml document.
-   *
-   * @param is the is
-   * @return the document
-   * @throws SAXException                 the sax exception
-   * @throws IOException                  the io exception
-   * @throws ParserConfigurationException the parser configuration exception
-   */
-  public static Document readXml(InputStream is) throws SAXException, IOException,
-      ParserConfigurationException {
-    DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-
-    dbf.setValidating(false);
-    dbf.setIgnoringComments(false);
-    dbf.setIgnoringElementContentWhitespace(true);
-    dbf.setNamespaceAware(true);
-
-    DocumentBuilder db = null;
-    db = dbf.newDocumentBuilder();
-    db.setEntityResolver(new NullResolver());
-
-    return db.parse(is);
-  }
-
-  private InputStream getInputStream(String schema) throws Exception {
-    InputStream sc = null;
-    if (Files.exists(Paths.get(schema))) {
-      // Look in local dir
-      //System.out.println("Schematron rules found in local dir");
-      sc = new FileInputStream(schema);
-    } else {
-      Class cls = Schematron.class;
-      ClassLoader cLoader = cls.getClassLoader();
-      sc = cLoader.getResourceAsStream(schema);
-    }
-    return sc;
-  }
-
-  /**
-   * Test xml string.
-   *
-   * @param xmlFileOriginal the xml file
-   * @param content  the content
-   * @return the string
-   * @throws Exception the exception
-   */
-  public String testXML(String xmlFileOriginal, String content) throws Exception {
-    String result;
-    try {
-      String xmlFile = xmlFileOriginal;
-      if (!xmlFileOriginal.contains("<?xml version"))
-        xmlFile = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>" + xmlFile;
-      InputStream sc = new StringInputStream(content);
-      TransformerFactory factory = new TransformerFactoryImpl();
-      factory.setURIResolver(new ClassPathURIResolver(Constants.SCHEMATRON_TEMPLATES_ROOT_DIR));
-      Templates rules = TemplatesFactory.newInstance().newTemplates(sc);
-      SchematronProcessor proc = SchematronProcessorFactory.newScehamtronEngine(rules);
-      result = proc.validate(xmlFile);
-      result = result.substring(0, result.indexOf("<!--")) + result.substring(result.indexOf("-->")+3);
-    } catch (Exception ex) {
-      ex.printStackTrace();
-      result = "";
-    }
-    return result;
   }
 }
