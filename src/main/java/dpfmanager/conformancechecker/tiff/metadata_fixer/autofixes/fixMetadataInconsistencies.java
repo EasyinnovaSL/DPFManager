fb8049d7c8a89c7a8c864f6809d290e60031f61e
Victor Muñoz <victormunoz@easyinnova.com>
2017-05-24 13:57:02 +0200

    Fixes #427 and #423

--- src/main/java/dpfmanager/conformancechecker/tiff/metadata_fixer/autofixes/fixMetadataInconsistencies.java
+++ src/main/java/dpfmanager/conformancechecker/tiff/metadata_fixer/autofixes/fixMetadataInconsistencies.java
@@ -25,6 +25,7 @@ import com.easyinnova.tiff.model.types.ExifIFD;
 import com.easyinnova.tiff.model.types.IFD;
 import com.easyinnova.tiff.model.types.XMP;
 import com.easyinnova.tiff.model.types.IPTC;
+import com.easyinnova.tiff.model.types.abstractTiffType;
 
 import java.util.Date;
 
@@ -56,13 +57,16 @@ public class fixMetadataInconsistencies implements autofix {
     IFD exif = null;
 
     if (ifd != null && ifd.containsTagId(TiffTags.getTagId("XMP"))) {
-      xmp = (XMP) ifd.getTag("XMP").getValue().get(0);
+      abstractTiffType obj = ifd.getTag("XMP").getValue().get(0);
+      if (obj instanceof XMP) xmp = (XMP)obj;
     }
     if (ifd != null && ifd.containsTagId(TiffTags.getTagId("IPTC"))) {
-      iptc = (IPTC)ifd.getTag("IPTC").getValue().get(0);
+      abstractTiffType obj = ifd.getTag("IPTC").getValue().get(0);
+      if (obj instanceof IPTC) iptc = (IPTC)obj;
     }
     if (ifd != null && ifd.containsTagId(TiffTags.getTagId("ExifIFD"))) {
-      exif = (IFD) ifd.getTag("ExifIFD").getValue().get(0);
+      abstractTiffType obj = ifd.getTag("ExifIFD").getValue().get(0);
+      if (obj instanceof IFD) exif = (IFD)obj;
     }
 
     String creatorXmp = null;
