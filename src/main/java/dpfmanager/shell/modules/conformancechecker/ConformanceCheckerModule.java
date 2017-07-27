901c3a3f60cc30f4cd68a7ba4aa4e6dbca91a293
Adrià Llorens <adria_llo@hotmail.com>
2017-07-27 10:39:52 +0200

    Fix create full from samll global report

--- src/main/java/dpfmanager/shell/modules/conformancechecker/ConformanceCheckerModule.java
+++ src/main/java/dpfmanager/shell/modules/conformancechecker/ConformanceCheckerModule.java
@@ -75,13 +75,12 @@ public class ConformanceCheckerModule extends DpfModule {
               getContext().send(BasicConfig.MODULE_MESSAGE, new AlertMessage(AlertMessage.Type.ERROR, DPFManagerProperties.getBundle().getString("errorReadingConfFile")));
               return;
             }
-            config.setQuick(cm.isQuick());
           } else {
             config = new Configuration();
             config.setDefault();
-            config.setQuick(cm.isQuick());
           }
         }
+        config.setQuick(cm.isQuick());
         service.initProcessInputRun(input, config, cm.getRecursive());
       }
     } else if (dpfMessage.isTypeOf(ProcessInputMessage.class)) {
