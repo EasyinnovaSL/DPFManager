d5a6e24cebe8d6f31eb91b128db9b90ba8ffa80e
Victor Muñoz <victormunoz@easyinnova.com>
2017-07-28 12:01:15 +0200

    training 96%, test 94%

--- src/test/java/dpfmanager/implementationchecker/RunsTest.java
+++ src/test/java/dpfmanager/implementationchecker/RunsTest.java
@@ -68,39 +68,47 @@ public class RunsTest extends TestCase {
         if (rr.toString().contains("TIFF signature is not correct")) classes.add(1);
         if (rr.toString().contains("cardinality")) classes.add(2);
         if (rr.toString().contains("but it is compatible")) classes.add(3);
-        if (rr.toString().contains("Tag type is not valid")) classes.add(4);
+        if (rr.toString().contains("type is not valid")) classes.add(4);
         if (rr.toString().contains("Incorrect Extra Samples Count") || rr.toString().contains("BitsPerSample must have a cardinality according to BitsPerSample")) classes.add(5);
         if (rr.toString().contains("Image width and image height do not match with strips sizes")) classes.add(6);
         if (rr.toString().contains("must have tag X Resolution") || rr.toString().contains("must have tag Y Resolution") || rr.toString().contains("Resolution tag must be greater than zero")) classes.add(7);
         if (rr.toString().contains("must have a Photometric Interpretation tag") || rr.toString().contains("must have a Width tag") || rr.toString().contains("must have a Length tag") || rr.toString().contains("must have a XResolution tag") || rr.toString().contains("must have a YResolution tag")) classes.add(8);
-        if (rr.toString().contains("Incorrect page number")) classes.add(9);
+        if (rr.toString().contains("Incorrect page number") || rr.toString().contains("Incoherent page number") || rr.toString().contains("invalid total number of pages")) classes.add(9);
         if (rr.toString().contains("type is expected")) classes.add(10);
         if (rr.toString().contains("TIFF signature must be 42")) classes.add(11);
         if (rr.toString().contains("Byte Order")) classes.add(12);
         if (rr.toString().contains("word alignment")) classes.add(13);
-        if (rr.toString().contains("ad offset") || rr.toString().contains("Duplicate pointer")) classes.add(14);
+        if (rr.toString().contains("ad offset") || rr.toString().contains("Duplicate pointer") || rr.toString().contains("Data already referenced")) classes.add(14);
         if (rr.toString().contains("must have at least one entry")) classes.add(15);
         if (rr.toString().contains("must have at least one Image File Directory")) classes.add(16);
         if (rr.toString().contains("strict ascending order")) classes.add(17);
         if (rr.toString().contains("Photometric Interpretation value must be between")) classes.add(18);
         if (rr.toString().contains("Strips offsets are invalid") || rr.toString().contains("Inconsistent strip lengths")) classes.add(19);
-        if (rr.toString().contains("must have the tag Strip")) classes.add(20);
+        if (rr.toString().contains("must have the tag Strip") || rr.toString().contains("RowsPerStrip is required")) classes.add(20);
         if (rr.toString().contains("Image width and image height do not match with strips sizes")) classes.add(21);
         if (rr.toString().contains("Tiles offsets are invalid") || rr.toString().contains("Inconsistent tile lengths")) classes.add(22);
-        if (rr.toString().contains("must have the tag Tile")) classes.add(23);
+        if (rr.toString().contains("must have the tag Tile") || rr.toString().contains("Missing required TileByteCounts tag for tiled images") || rr.toString().contains("Missing required TileOffsets tag for tiled images")) classes.add(23);
         if (rr.toString().contains("Invalid value for field TileWidth") || rr.toString().contains("Invalid value for field TileLength")) classes.add(24);
         if (rr.toString().contains("Image width and image height do not match with tiles sizes") || rr.toString().contains("TileWidth greater than the ImageWidth") || rr.toString().contains("TileLength bigger than the ImageLength")) classes.add(25);
-        if (rr.toString().contains("should only be used in") && getPhotometric(tr) == 0) classes.add(26); //bilevel
+        if ((rr.toString().contains("should only be used in")) && getPhotometric(tr) == 0) classes.add(26); //bilevel
+        if (rr.toString().contains("Invalid Compression for Bilevel image")) classes.add(26); //bilevel
         if (rr.toString().contains("should only be used in") && getPhotometric(tr) == 1) classes.add(27);
+        if (rr.toString().contains("CCITT compression is defined only for bilevel") && getPhotometric(tr) == 1) classes.add(27);
         if ((rr.toString().contains("should only be used in") && getPhotometric(tr) == 3) || rr.toString().contains("Color Map must be included in palette color images")) classes.add(28); //palette
+        if (rr.toString().contains("CCITT compression is defined only for bilevel") && getPhotometric(tr) == 2) classes.add(28);
         if ((rr.toString().contains("should only be used in") && getPhotometric(tr) == 4) ||
             rr.toString().contains("NewSubfileType defines a transparency mask while PhotometricInterpretation defines another image type") ||
             rr.toString().contains("Invalid Bits per Sample for Transparency Mask image") ||
             rr.toString().contains("Invalid Samples per Pixel for Transparency Mask image")) classes.add(29); //transparency mask
+        if (rr.toString().contains("CCITT compression is defined only for bilevel") && getPhotometric(tr) == 4) classes.add(29);
         if (rr.toString().contains("should only be used in") && getPhotometric(tr) == 5) classes.add(30); //cmyk
+        if (rr.toString().contains("CCITT compression is defined only for bilevel") && getPhotometric(tr) == 5) classes.add(30);
         if ((rr.toString().contains("should only be used in") && getPhotometric(tr) == 6) || rr.toString().contains("inappropriate for Class Y")) classes.add(31); //ycbcr
+        if (rr.toString().contains("CCITT compression is defined only for bilevel") && getPhotometric(tr) == 6) classes.add(31);
         if (rr.toString().contains("should only be used in") && getPhotometric(tr) == 8) classes.add(32); //cielab
+        if (rr.toString().contains("CCITT compression is defined only for bilevel") && getPhotometric(tr) == 8) classes.add(32);
         if (rr.toString().contains("should only be used in") && getPhotometric(tr) == 2) classes.add(33); //rgb
+        if (rr.toString().contains("CCITT compression is defined only for bilevel") && getPhotometric(tr) == 2) classes.add(33);
         if (rr.toString().contains("Only 7-bit ASCII codes are accepted") || rr.toString().contains("ASCII strings must terminate with NUL")) classes.add(34);
         if (rr.toString().contains("DateTime cardinality is not valid") || rr.toString().contains("Incorrect format for DateTime")) classes.add(35);
         if (rr.toString().contains("More than 10 private tags")) classes.add(36);
@@ -139,7 +147,7 @@ public class RunsTest extends TestCase {
       long photo = tr.getModel().getFirstIFD().getTag("PhotometricInterpretation").getFirstNumericValue();
       if (photo == 0 || photo == 1)
       {
-        if (tr.getModel().getFirstIFD().getTag("PhotometricInterpretation").getFirstNumericValue() == 1) {
+        if (tr.getModel().getFirstIFD().getTag("BitsPerSample").getFirstNumericValue() == 1) {
           photo = 0;
         } else {
           photo = 1;
@@ -170,6 +178,7 @@ public class RunsTest extends TestCase {
           String sClass = dirClass.getName();
           int iClass = Integer.parseInt(sClass.substring(2));
           for (File fileClass : dirClass.listFiles()) {
+            if (fileClass.getName().toLowerCase().contains("thumbs.db")) continue;
             boolean policy = policyClass == iClass;
             boolean tia = tiaClasses.contains(iClass);
             boolean ep = epClasses.contains(iClass);
